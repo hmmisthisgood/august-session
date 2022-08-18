afdfeb587b2e41ae01dd8b9d318ec9f7eb8f121c
hmmisthisgood <test@test.com>
2022-08-18 18:14:59 +0545

    update the instagram ui

--- first_app/lib/screen/insta_post.dart
+++ first_app/lib/screen/insta_post.dart
@@ -13,39 +13,76 @@ class InstaPost extends StatelessWidget {
         children: [
           /// username and profile picture
 
-          Container(
-            color: Colors.yellow,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(40),
-                          child: Image.network(
-                            image,
-                            height: 40,
-                            width: 40,
-                            fit: BoxFit.cover,
-                          )),
-                      SizedBox(width: 15),
-                      Text("IAmBijay"),
-                    ],
-                  ),
-                  Icon(Icons.more_horiz)
-                ],
-              ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(40),
+                        child: Image.network(
+                          image,
+                          height: 40,
+                          width: 40,
+                          fit: BoxFit.cover,
+                        )),
+                    SizedBox(width: 15),
+                    Text("IAmBijay"),
+                  ],
+                ),
+                Icon(Icons.more_horiz)
+              ],
             ),
           ),
 
           /// actual post image
           ///
-          Image.network(image)
+          Image.network(image),
           //// like comment share buttons
 
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.favorite_border_rounded,
+                      size: 30,
+                    ),
+                    SizedBox(width: 15),
+                    Icon(
+                      Icons.comment,
+                      size: 30,
+                    ),
+                    SizedBox(width: 15),
+                    Icon(
+                      Icons.send,
+                      size: 30,
+                    ),
+                  ],
+                ),
+                Icon(
+                  Icons.bookmark,
+                  size: 30,
+                ),
+              ],
+            ),
+          ),
+
           /// caption
+
+          Row(
+            children: [
+              Text("Liked By"),
+              Text(" arjun and others",
+                  style: TextStyle(fontWeight: FontWeight.bold))
+            ],
+          ),
+          Text("18 Aug", style: TextStyle(color: Colors.grey))
         ],
       ),
     );
